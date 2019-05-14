13f58566965bb1d5bc919e38eeef04ad4f818632
Roberto D'Auria <roberto@measurementlab.net>
2019-05-14 16:46:19 +0200

    Add -add and refactor main.go.

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/m-lab/go/flagx"
@@ -21,6 +23,15 @@ const (
 var (
 	node      = flag.String("node", "", "The node's name.")
 	projectID = flag.String("project", defaultProjectID, "Project ID to use.")
+	bmcUser   = flag.String("bmcuser", "",
+		"BMC username (for adding or updating a BMC.)")
+	bmcPass = flag.String("bmcpassword", "",
+		"BMC password (for adding or updating a BMC.)")
+	bmcAddr = flag.String("addr", "",
+		"BMC IP address (for adding or updating a BMC.)")
+
+	// Actions
+	addAction = flag.Bool("add", false, "Add a new node to GCS.")
 
 	// These allow for testing.
 	// TODO: remove this once https://github.com/m-lab/reboot-service/issues/12
@@ -37,6 +48,59 @@ func usage() {
 	osExit(1)
 }
 
+func add() error {
+	// Add a new BMC to Google Cloud Datastore
+	var err error
+	if *bmcUser == "" || *bmcPass == "" || *bmcAddr == "" {
+		return errors.New("bmcuser, bmcpass and bmcaddr are required")
+	}
+
+	creds := &creds.Credentials{
+		Address:  *bmcAddr,
+		Hostname: *node,
+		Model:    "DRAC",
+		Username: *bmcUser,
+		Password: *bmcPass,
+	}
+
+	log.Printf("Adding credentials for host %v\n", *node)
+	provider := credsNewProvider(*projectID, namespace)
+
+	// Provider.AddCredentials will create the entity regardless of whether it
+	// exists already or not, so we need to explicitly check to prevent
+	// overriding the existing entity by mistake.
+	_, err = provider.FindCredentials(context.Background(), *node)
+	if err == nil {
+		return fmt.Errorf("Credentials for hostname %v already exist "+
+			"- did you mean -update?", *node)
+	}
+
+	err = provider.AddCredentials(context.Background(), *node, creds)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fetch() error {
+	// Default behavior (if no other actions have been specified) is to fetch
+	// credentials for the selected node.
+	provider := credsNewProvider(*projectID, namespace)
+	creds, err := provider.FindCredentials(context.Background(), *node)
+	if err != nil {
+		return err
+	}
+
+	jsonOutput, err := json.MarshalIndent(creds, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(jsonOutput))
+	return nil
+}
+
 func main() {
 	flag.Usage = usage
 	flag.Parse()
@@ -48,12 +112,14 @@ func main() {
 		flag.Usage()
 	}
 
-	provider := credsNewProvider(*projectID, namespace)
-	creds, err := provider.FindCredentials(context.Background(), *node)
-	rtx.Must(err, "Error while fetching credentials")
-
-	jsonOutput, err := json.MarshalIndent(creds, "", "  ")
-	rtx.Must(err, "Cannot marshal JSON")
-
-	fmt.Println(string(jsonOutput))
+	// Handle action flags
+	//
+	// -add: create a new entity
+	// -update: updates an existing entity (TODO)
+	// no flags: fetch credentials
+	if *addAction {
+		rtx.Must(add(), "Error while adding node")
+	} else {
+		rtx.Must(fetch(), "Error while fetching credentials")
+	}
 }
