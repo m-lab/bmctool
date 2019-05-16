9b09a7bf5396f93ecb37353d06fac55248ef7eea
Roberto D'Auria <roberto@measurementlab.net>
2019-05-16 23:39:25 +0200

    Implement -add to enable creation of new Credentials entites on GCD (#3)
    * Add -add and refactor main.go.
    
    * Fix tests.
    
    * Add TODO.
    
    * Improve tests and print credentials upon successful creation.
    
    * Print creds after successful creation on GCD.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/apex/log"
 	"github.com/m-lab/go/flagx"
 	"github.com/m-lab/go/rtx"
 
@@ -21,10 +22,17 @@ const (
 var (
 	node      = flag.String("node", "", "The node's name.")
 	projectID = flag.String("project", defaultProjectID, "Project ID to use.")
+	bmcUser   = flag.String("bmcuser", "",
+		"BMC username (for adding or updating a BMC.)")
+	bmcPass = flag.String("bmcpassword", "",
+		"BMC password (for adding or updating a BMC.)")
+	bmcAddr = flag.String("addr", "",
+		"BMC IP address (for adding or updating a BMC.)")
+
+	// Actions
+	addAction = flag.Bool("add", false, "Add a new node to GCS.")
 
 	// These allow for testing.
-	// TODO: remove this once https://github.com/m-lab/reboot-service/issues/12
-	// is closed.
 	credsNewProvider = creds.NewProvider
 	osExit           = os.Exit
 )
@@ -35,6 +43,56 @@ func usage() {
 	fmt.Fprintln(os.Stderr, "Flags:")
 	flag.PrintDefaults()
 	osExit(1)
+}
+
+// addCredentials adds a new BMC to Google Cloud Datastore.
+func addCredentials() error {
+	if *bmcUser == "" || *bmcPass == "" || *bmcAddr == "" {
+		log.Error("bmcuser, bmcpassword and addr are required")
+		osExit(1)
+	}
+
+	creds := &creds.Credentials{
+		Address:  *bmcAddr,
+		Hostname: *node,
+		Model:    "DRAC",
+		Username: *bmcUser,
+		Password: *bmcPass,
+	}
+
+	log.Infof("Adding credentials for host %v\n", *node)
+	provider := credsNewProvider(*projectID, namespace)
+
+	// Provider.AddCredentials will create the entity regardless of whether it
+	// exists already or not, so we need to explicitly check to prevent
+	// overriding the existing entity by mistake.
+	_, err := provider.FindCredentials(context.Background(), *node)
+	if err == nil {
+		log.Errorf("Credentials for hostname %v already exist\n", *node)
+		osExit(1)
+	}
+
+	rtx.Must(provider.AddCredentials(context.Background(), *node, creds),
+		"Error while adding Credentials")
+
+	jsonOutput, err := json.MarshalIndent(creds, "", "  ")
+	rtx.Must(err, "Cannot marshal JSON output")
+
+	fmt.Println(string(jsonOutput))
+	return nil
+}
+
+// printCredentials retrieves credentials for a given hostname and prints them
+// in JSON format.
+func printCredentials(host string) {
+	provider := credsNewProvider(*projectID, namespace)
+	creds, err := provider.FindCredentials(context.Background(), *node)
+	rtx.Must(err, "Cannot fetch credentials")
+
+	jsonOutput, err := json.MarshalIndent(creds, "", "  ")
+	rtx.Must(err, "Cannot marshal JSON output")
+
+	fmt.Println(string(jsonOutput))
 }
 
 func main() {
@@ -48,12 +106,18 @@ func main() {
 		flag.Usage()
 	}
 
-	provider := credsNewProvider(*projectID, namespace)
-	creds, err := provider.FindCredentials(context.Background(), *node)
-	rtx.Must(err, "Error while fetching credentials")
-
-	jsonOutput, err := json.MarshalIndent(creds, "", "  ")
-	rtx.Must(err, "Cannot marshal JSON")
-
-	fmt.Println(string(jsonOutput))
+	// Handle action flags
+	// TODO(roberto): use a package that handles subcommands (such as kingpin)
+	// instead of "flag".
+	//
+	// -add: create a new entity
+	// -update: updates an existing entity (TODO)
+	// no flags: fetch credentials
+	if *addAction {
+		rtx.Must(addCredentials(), "Error while adding credentials")
+	} else {
+		// Default behavior (if no other actions have been specified) is to fetch
+		// credentials for the requested node.
+		printCredentials(*node)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -2,38 +2,26 @@ package main
 
 import (
 	"context"
-	"errors"
 	"testing"
+
+	"github.com/m-lab/reboot-service/creds/credstest"
 
 	"github.com/m-lab/go/osx"
 	"github.com/m-lab/reboot-service/creds"
 	"github.com/stretchr/testify/assert"
 )
 
-type providerMock struct {
-	returnValue *creds.Credentials
-	returnErr   bool
-}
-
-func (p *providerMock) FindCredentials(ctx context.Context, node string) (*creds.Credentials, error) {
-	if p.returnErr {
-		return nil, errors.New("error while fetching credentials")
-	}
-
-	return p.returnValue, nil
-}
-
 func Test_main(t *testing.T) {
 	// Create fake Credentials.
 	fakeCreds := &creds.Credentials{
-		Address:  "0.0.0.0",
+		Address:  "127.0.0.1",
 		Hostname: "mlab4.lga0t.measurement-lab.org",
 		Username: "username",
 		Password: "password",
-		Model:    "drac",
+		Model:    "DRAC",
 	}
 
-	// Replace osExit and logFatalf so that tests don't stop running.
+	// Replace osExit so that tests don't stop running.
 	osExit = func(code int) {
 		if code != 1 {
 			t.Fatalf("Expected a 1 exit code, got %d.", code)
@@ -44,23 +32,63 @@ func Test_main(t *testing.T) {
 
 	oldCredsNewProvider := credsNewProvider
 
+	// No node specified, just print usage and return.
 	t.Run("failure-no-node-provided", func(t *testing.T) {
-		// No node specified, just print usage and return.
 		assert.PanicsWithValue(t, "os.Exit called", main, "os.Exit was not called")
 	})
 
-	// Set up env variables to simulate flags.
-	restoreNode := osx.MustSetenv("NODE", "mlab4.lga0t.measurement-lab.org")
-	defer restoreNode()
-
-	credsNewProvider = func(projectID string, namespace string) creds.Provider {
-		return &providerMock{
-			returnValue: fakeCreds,
-		}
+	// Set up a FakeProvider with fake credentials.
+	prov := credstest.NewProvider()
+	prov.AddCredentials(context.Background(), "mlab4d.lga0t.measurement-lab.org", fakeCreds)
+	credsNewProvider = func(string, string) creds.Provider {
+		return prov
 	}
-	t.Run("success", func(t *testing.T) {
+
+	// Set up env variables to simulate flags.
+	restoreNode := osx.MustSetenv("NODE", "mlab4d.lga0t.measurement-lab.org")
+	defer restoreNode()
+	t.Run("success-print-credentials", func(t *testing.T) {
 		main()
 	})
 
+	// main() should successfully add a new node.
+	osx.MustSetenv("NODE", "mlab1d.lga0t.measurement-lab.org")
+	restoreAdd := osx.MustSetenv("ADD", "1")
+	restoreUser := osx.MustSetenv("BMCUSER", "username")
+	restorePass := osx.MustSetenv("BMCPASSWORD", "password")
+	restoreAddr := osx.MustSetenv("ADDR", "127.0.0.2")
+	defer restoreAdd()
+	defer restoreUser()
+	defer restorePass()
+	defer restoreAddr()
+	t.Run("success-node-added", func(t *testing.T) {
+		main()
+	})
+
+	// Check the node that's been just added.
+	c, err := prov.FindCredentials(context.Background(), "mlab1d.lga0t.measurement-lab.org")
+	if err != nil {
+		t.Errorf("FindCredentials() returned error: %v", err)
+	}
+	if c.Hostname != "mlab1d.lga0t.measurement-lab.org" ||
+		c.Username != "username" || c.Password != "password" ||
+		c.Address != "127.0.0.2" || c.Model != "DRAC" {
+		t.Errorf("AddCredentials() didn't add the expected entity: %v", c)
+	}
+
+	// main() should exit if the node already exists.
+	osx.MustSetenv("NODE", "mlab4d.lga0t.measurement-lab.org")
+	t.Run("failure-add-node-already-exists", func(t *testing.T) {
+		assert.PanicsWithValue(t, "os.Exit called", main,
+			"os.Exit was not called")
+	})
+
+	osx.MustSetenv("BMCUSER", "")
+	// main() should fail when trying to -add without specifying the required
+	// arguments (username, password and address).
+	t.Run("failure-add-missing-args", func(t *testing.T) {
+		assert.PanicsWithValue(t, "os.Exit called", main,
+			"os.Exit was not called")
+	})
 	credsNewProvider = oldCredsNewProvider
 }
