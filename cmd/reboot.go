b60f78d761941f8e55cfc1ca150086bb8da70557
Roberto D'Auria <roberto@measurementlab.net>
2019-06-14 19:00:15 +0200

    Implements the bmctool reboot command (#15)
    * Implement shorthand BMC hostnames.
    
    * Test makeBMCHostname.
    
    * Add test case.
    
    * Add reboot command.
    
    * Add test for the reboot command.
    
    * Check that the Reboot API URL has been provided.
    
    * Restore old values for osExit and httpClient.
    
    * Check resp.StatusCode and add corresponding test cases.

---
+++ cmd/reboot.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/apex/log"
+	"github.com/m-lab/go/rtx"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	rebootEndpoint = "/v1/reboot"
+	defaultTimeout = 1 * time.Minute
+)
+
+var (
+	rebootAPIURL string
+
+	// rebootCmd represents the reboot command
+	rebootCmd = &cobra.Command{
+		Use:   "reboot <hostname>",
+		Short: "Reboot a BMC using the Reboot API",
+		Long: `This command sends a POST request to the Reboot API to reboot the provided node.
+
+The reboot-api-url flag can be also provided via the REBOOTAPIURL environment variable.`,
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			reboot(args[0])
+		},
+	}
+
+	httpClient = &http.Client{
+		Timeout: defaultTimeout,
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(rebootCmd)
+
+	viper.AutomaticEnv()
+
+	rebootCmd.Flags().StringVar(&rebootAPIURL, "reboot-api-url",
+		viper.GetString("REBOOTAPIURL"), "Reboot API URL")
+}
+
+func reboot(host string) {
+	// Make sure the Reboot API URL has been provided.
+	if rebootAPIURL == "" {
+		log.Error("The Reboot API URL must be specified (see bmctool help reboot).")
+		osExit(1)
+	}
+	// Make sure the provided host is a valid M-Lab BMC.
+	host = makeBMCHostname(host)
+	fullURL := rebootAPIURL + rebootEndpoint + "?host=" + host
+
+	log.Infof("POST %s", fullURL)
+	resp, err := httpClient.Post(fullURL, "text/plain", nil)
+	rtx.Must(err, "Cannot send reboot request")
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	rtx.Must(err, "Cannot read response from %s")
+
+	if resp.StatusCode != http.StatusOK {
+		log.Errorf("Reboot failed (status code %d): %s", resp.StatusCode, string(body))
+		osExit(1)
+	}
+
+	log.Infof(string(body))
+}
---
+++ cmd/reboot_test.go
@@ -0,0 +1,83 @@
+package cmd
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type RoundTripFunc func(req *http.Request) *http.Response
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+// NewTestClient returns a *http.Client with Transport replaced to avoid making
+// real calls.
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: RoundTripFunc(fn),
+	}
+}
+
+func Test_reboot(t *testing.T) {
+	// Replace osExit so that tests don't stop running.
+	oldOsExit := osExit
+	osExit = func(code int) {
+		if code != 1 {
+			t.Fatalf("Expected a 1 exit code, got %d.", code)
+		}
+
+		panic("os.Exit called")
+	}
+	// If REBOOTAPIURL isn't set, reboot() should fail.
+	rebootAPIURL = ""
+	assert.PanicsWithValue(t, "os.Exit called", func() {
+		reboot("mlab1d.lga0t.measurement-lab.org")
+	}, "os.Exit was not called")
+	rebootAPIURL = "dummy"
+
+	// Set up a http.Client that returns values useful for testing.
+	oldHTTPClient := httpClient
+	httpClient = NewTestClient(func(req *http.Request) *http.Response {
+		if strings.Contains(req.URL.String(), "mlab4d") {
+			return &http.Response{
+				StatusCode: http.StatusInternalServerError,
+				Body: ioutil.NopCloser(bytes.NewBufferString(
+					"This is an error.")),
+				Header: make(http.Header),
+			}
+		}
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body: ioutil.NopCloser(bytes.NewBufferString(
+				"Server power operation successful.")),
+			Header: make(http.Header),
+		}
+	})
+
+	// Rebooting an mlab4 will always fail.
+	assert.PanicsWithValue(t, "os.Exit called", func() {
+		reboot("mlab4d.lga0t.measurement-lab.org")
+	}, "os.Exit was not called")
+
+	// Successful reboot, stdout should contain the expected message.
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	reboot("mlab1d.lga0t.measurement-lab.org")
+	log.SetOutput(os.Stderr)
+
+	if !strings.Contains(buf.String(), "Server power operation successful.") {
+		t.Errorf("Unexpected output: %s", buf.String())
+	}
+
+	httpClient = oldHTTPClient
+	osExit = oldOsExit
+
+}
